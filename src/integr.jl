145572679ece350142dedac4a047a99eff73f03b
SImone <simone.luciano21@gmail.com>
2020-02-23 16:35:37 +0100

    distributed and threads

--- src/integr.jl
+++ src/integr.jl
@@ -11,8 +11,8 @@ end
 
 """ The main integration routine """
 function TT(
-tau::Array{Float64,2}, 
-alpha::Int, beta::Int, gamma::Int, 
+tau::Array{Float64,2},
+alpha::Int, beta::Int, gamma::Int,
 signedInt::Bool=false)
 	vo,va,vb = tau[:,1],tau[:,2],tau[:,3]
 	a = va - vo
@@ -22,19 +22,19 @@ signedInt::Bool=false)
 		for k=0:beta
 			for m=0:gamma
 				s2 = 0.0
-				for i=0:h 
+				for i=0:h
 					s3 = 0.0
 					for j=0:k
 						s4 = 0.0
 						for l=0:m
-							s4 += binomial(m,l) * a[3]^(m-l) * b[3]^l * M( 
+							s4 += binomial(m,l) * a[3]^(m-l) * b[3]^l * M(
 								h+k+m-i-j-l, i+j+l )
 						end
 						s3 += binomial(k,j) * a[2]^(k-j) * b[2]^j * s4
 					end
 					s2 += binomial(h,i) * a[1]^(h-i) * b[1]^i * s3;
 				end
-				s1 += binomial(alpha,h) * binomial(beta,k) * binomial(gamma,m) * 			
+				s1 += binomial(alpha,h) * binomial(beta,k) * binomial(gamma,m) *
 						vo[1]^(alpha-h) * vo[2]^(beta-k) * vo[3]^(gamma-m) * s2
 			end
 		end
@@ -44,10 +44,93 @@ signedInt::Bool=false)
 		return s1 * norm(c) * sign(c[3])
 	else
 		return s1 * norm(c)
-	end	
+	end
 end
 
-""" 
+
+
+using LinearAlgebraicRepresentation
+Lar = LinearAlgebraicRepresentation
+using Distributed
+using BenchmarkTools
+tau=[0.0 1.0 0.0; 0.0 0.0 1.0; 0.0 0.0 0.0];
+
+
+function TT_distributed(
+tau::Array{Float64,2},
+alpha::Int, beta::Int, gamma::Int,
+signedInt::Bool=false)
+	vo,va,vb = tau[:,1],tau[:,2],tau[:,3]
+	a = va - vo
+	b = vb - vo
+	s1 = 0.0
+    for h=0:alpha
+		for k=0:beta
+            s1 = @distributed (+) for m=0:gamma
+				s2 = 0.0
+                for i=0:h
+					s3 = 0.0
+					for j=0:k
+						s4 = 0.0
+						for l=0:m
+							s4 += binomial(m,l) * a[3]^(m-l) * b[3]^l * M(
+								h+k+m-i-j-l, i+j+l )
+						end
+						s3 += binomial(k,j) * a[2]^(k-j) * b[2]^j * s4
+					end
+					s2 += binomial(h,i) * a[1]^(h-i) * b[1]^i * s3;
+				end
+				binomial(alpha,h) * binomial(beta,k) * binomial(gamma,m) *
+						vo[1]^(alpha-h) * vo[2]^(beta-k) * vo[3]^(gamma-m) * s2
+			end
+		end
+	end
+	c = cross(a,b)
+	if signedInt == true
+		return s1 * norm(c) * sign(c[3])
+	else
+		return s1 * norm(c)
+	end
+end
+function TT_threads(
+tau::Array{Float64,2},
+alpha::Int, beta::Int, gamma::Int,
+signedInt::Bool=false)
+	vo,va,vb = tau[:,1],tau[:,2],tau[:,3]
+	a = va - vo
+	b = vb - vo
+	s1 = 0.0
+    vec=Vector{Float64}(undef,100000000)
+    Threads.@threads for h=0:alpha
+		for k=0:beta
+            for m=0:gamma
+				s2 = 0.0
+                for i=0:h
+					s3 = 0.0
+					for j=0:k
+						s4 = 0.0
+						for l=0:m
+							s4 += binomial(m,l) * a[3]^(m-l) * b[3]^l * M(
+								h+k+m-i-j-l, i+j+l )
+						end
+						s3 += binomial(k,j) * a[2]^(k-j) * b[2]^j * s4
+					end
+					s2 += binomial(h,i) * a[1]^(h-i) * b[1]^i * s3;
+				end
+				vec[h+1]=binomial(alpha,h) * binomial(beta,k) * binomial(gamma,m) *
+						vo[1]^(alpha-h) * vo[2]^(beta-k) * vo[3]^(gamma-m) * s2
+			end
+		end
+	end
+    s1=sum(vec)
+	c = cross(a,b)
+	if signedInt == true
+		return s1 * norm(c) * sign(c[3])
+	else
+		return s1 * norm(c)
+	end
+end
+"""
 	II(P::Lar.LAR, alpha::Int, beta::Int, gamma::Int, signedInt=false)
 
 Basic integration function on 2D plane.
@@ -72,8 +155,8 @@ julia> Lar.II(P, 0,0,0)
 ```
 """
 function II(
-P::LAR, 
-alpha::Int, beta::Int, gamma::Int, 
+P::LAR,
+alpha::Int, beta::Int, gamma::Int,
 signedInt=false)::Float64
     w = 0
     V, FV = P
@@ -94,11 +177,11 @@ signedInt=false)::Float64
         else
         	println("ERROR: FV[$(i)] is degenerate")
         end
-    end    
+    end
     return w
 end
 
-""" 
+"""
 	III(P::Lar.LAR, alpha::Int, beta::Int, gamma::Int)::Float64
 
 Basic integration function on 3D space.
@@ -119,7 +202,7 @@ julia> FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]]
  [2, 3, 4]
 
 julia> P = V,FV
-([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0], 
+([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0],
 Array{Int64,1}[[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]])
 
 julia> Lar.III(P, 0,0,0)
@@ -164,7 +247,7 @@ julia> FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]]
  [2, 3, 4]
 
 julia> P = V,FV
-([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0], 
+([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0],
 Array{Int64,1}[[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]])
 
 julia> Lar.volume(P)
@@ -198,7 +281,7 @@ julia> FV = [[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]]
  [2, 3, 4]
 
 julia> P = V,FV
-([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0], 
+([0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0],
 Array{Int64,1}[[1, 2, 4], [1, 3, 2], [4, 3, 1], [2, 3, 4]])
 
 julia> Lar.volume(P)
@@ -238,12 +321,12 @@ end
 #		row = [faceVertPairs[face][1] for k=1:length(FE[face])]
 #		push!(triangles, vcat(faceVertPairs[face],row'))
 #        P = V,triangles[face]
-#        area = surface(P,false) 
+#        area = surface(P,false)
 #        push!(cochain,area)
 #    end
 #    return cochain
 #end
-    
+
 #    TODO: after having implemented ∂_3/∂_2
 #def signedIntSurfIntegration(model,signedInt=False)
 #    V,FV,EV = model
@@ -252,16 +335,16 @@ end
 #    for face in FV
 #        triangles = AA(C(AL)(face[0]))(TRANS([face[1-1],face[2]]))
 #        P = V,triangles
-#        area = Surface(P,signedInt) 
+#        area = Surface(P,signedInt)
 #        cochain += [area]
 #    return cochain
 #
 
 
-""" 
+"""
 	firstMoment(P::Lar.LAR)::Array{Float64,1}
 
-First moments as terms of the Euler tensor. Remember that the integration algorithm is a boundary integration. Hence the model must be a boundary model. In this case, a 2-complex of triangles. 
+First moments as terms of the Euler tensor. Remember that the integration algorithm is a boundary integration. Hence the model must be a boundary model. In this case, a 2-complex of triangles.
 
 # Example # unit 3D tetrahedron
 ```julia
@@ -288,7 +371,7 @@ end
 
 
 
-""" 
+"""
 	secondMoment(P::Lar.LAR)::Array{Float64,1}
 
 Second moments as terms of the Euler tensor.
@@ -318,7 +401,7 @@ end
 
 
 
-""" 
+"""
 	inertiaProduct(P::Lar.LAR)::Array{Float64,1}
 
 Inertia products as terms of the Euler tensor.
@@ -348,7 +431,7 @@ end
 
 
 
-""" 
+"""
 	centroid(P::Lar.LAR)::Array{Float64,1}
 
 Barycenter or `centroid` of polyhedron `P`.
@@ -374,7 +457,7 @@ end
 
 
 
-""" 
+"""
 	inertiaMoment(P::Lar.LAR)::Array{Float64,1}
 
 Inertia moments  of polyhedron `P`.
@@ -412,11 +495,11 @@ end
 
 
 """ Implementation using integr.jl """
-function chainAreas(V::Array{Float64,2}, EV::Array{Int64,2}, 
+function chainAreas(V::Array{Float64,2}, EV::Array{Int64,2},
 				chains::Array{Array{Int64,1},1})
 	if size(V,1) == 2
 		V = vcat(V,zeros(1,size(V,2)))
-	end	
+	end
 	pivots = [EV[:,abs(chain[1])][1] for chain in chains]
 	out = zeros(length(pivots))
 	for k=1:length(chains)
@@ -435,4 +518,3 @@ function chainAreas(V::Array{Float64,2}, EV::Array{Int64,2},
 	end
 	return out
 end
-
