43f1a53522cd33da695595b4eea7205fe2607e15
SImone <simone.luciano21@gmail.com>
2020-02-26 02:45:59 +0100

    minor

--- src/integr.jl
+++ src/integr.jl
@@ -474,7 +474,7 @@ function III_distributed(P::LAR, alpha::Int, beta::Int, gamma::Int)::Float64
     w = 0
     V, FV = P
     w = @distributed (+) for i=1:length(FV)
-        getVal(V,FV,i,alpha,beta,gamma)
+        Lar.getVal(V,FV,i,alpha,beta,gamma)
     end
     return w/(alpha + 1)
 end
@@ -482,7 +482,7 @@ function III_distributed_sync(P::LAR, alpha::Int, beta::Int, gamma::Int)::Float6
     w = 0
     V, FV = P
     w = @sync @distributed (+) for i=1:length(FV)
-        getVal(V,FV,i,alpha,beta,gamma)
+        Lar.getVal(V,FV,i,alpha,beta,gamma)
     end
     return w/(alpha + 1)
 end
